5d8523740a10af3b26f2f6d15dcaeb7f7e486fab
Reinardus Redo Haryono <rredoh.rrh@gmail.com>
2018-11-03 12:46:35 +0700

    Initial

---
+++ TouchEffectRepro/TouchEffectRepro/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TouchEffectRepro.Views;
+
+namespace TouchEffectRepro.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private bool can_click = true;
+
+        public DelegateCommand Command_Click => new DelegateCommand(Click).ObservesCanExecute(() => can_click);
+        
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        private async void Click()
+        {
+            //if (!can_click) return;
+
+            can_click = false;
+            await NavigationService.NavigateAsync(nameof(Page1), useModalNavigation:true);
+            can_click = true;
+        }
+    }
+}
